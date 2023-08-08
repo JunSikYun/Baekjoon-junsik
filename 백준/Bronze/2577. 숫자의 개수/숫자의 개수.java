d2eade97e055769f975f6056b08aa7c433e6b107
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2023-08-08 15:20:19 +0900

    [Bronze II] Title: 숫자의 개수, Time: 80 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 시간복잡도상 Systme.out.prinln
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer stk = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int[] list=new int[10];
+        int check=a*b*c;
+        String change=String.valueOf(check);
+        for(int i=0;i<change.length();i++){
+            int tmp=Character.getNumericValue(change.charAt(i));
+            list[tmp]++;
+        }
+        for(int j=0;j<list.length;j++){
+            System.out.println(list[j]);
+        }
+
+        // int b=Integer.parseInt(stk.nextToken());
+        // System.out.println(a+b);
+        // bw.write(String.valueOf(a-b));
+        // bw.newLine();//개행 넣어주기
+        // bw.write(String.valueOf(a));
+        // bw.flush();
+        // for(int i=0;i<TC;i++){
+        // StringTokenizer stk=new StringTokenizer(br.readLine());
+        // int a=Integer.parseInt(stk.nextToken());
+        // int b=Integer.parseInt(stk.nextToken());
+        // System.out.println(a-b);
+        // br.readLine();
+    }
+}
