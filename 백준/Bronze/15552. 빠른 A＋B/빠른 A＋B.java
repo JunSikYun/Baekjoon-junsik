b1a556f8b7d407ddec0cfa53b2dcb1388049c28c
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2023-08-10 13:12:25 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 884 ms, Memory: 298304 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 시간복잡도상 Systme.out.prinln
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk;
+        int a=Integer.parseInt(br.readLine());
+        for(int i=0;i<a;i++){
+            stk= new StringTokenizer(br.readLine()," ");
+            bw.write((Integer.parseInt(stk.nextToken())+(Integer.parseInt(stk.nextToken())))+"\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();;
+    }
+}
+// int b=Integer.parseInt(stk.nextToken());
+// System.out.println(a+b);
+// bw.write(String.valueOf(a-b));
+// bw.newLine();//개행 넣어주기
+// bw.write(String.valueOf(a));
+// bw.flush();
+// for(int i=0;i<TC;i++){
+// StringTokenizer stk=new StringTokenizer(br.readLine());
+// int a=Integer.parseInt(stk.nextToken());
+// int b=Integer.parseInt(stk.nextToken());
+// System.out.println(a-b);
+// br.readLine();
