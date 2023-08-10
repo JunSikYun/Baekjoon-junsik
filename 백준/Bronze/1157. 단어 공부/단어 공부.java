64881bb5565bc777daa34d97c877ff9ac88049e7
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2023-08-10 15:49:37 +0900

    [Bronze I] Title: 단어 공부, Time: 212 ms, Memory: 24452 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 시간복잡도상 Systme.out.prinln
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer stk=new StringTokenizer(br.readLine());
+        String input = br.readLine();
+        input = input.toLowerCase();
+        int[] list = new int[26];
+        for (int i = 0; i < input.length(); i++) {
+            list[(int) input.charAt(i)-97]++;
+        }
+        int check = -1;
+        char max = '?';
+        for (int j = 0; j < 26; j++) {
+            if (list[j] > check) {
+                check = list[j];
+                max = (char)(j+65);
+            }
+            else if(list[j]==check){
+                max='?';
+            }
+        }
+        System.out.println(max);
+    }
+}
+// 97~122 소문자 아스키 코드 값
+// int b=Integer.parseInt(stk.nextToken());
+// System.out.println(a+b);
+// bw.write(String.valueOf(a-b));
+// bw.newLine();//개행 넣어주기
+// bw.write(String.valueOf(a));
+// bw.flush();
+// for(int i=0;i<TC;i++){
+// StringTokenizer stk=new StringTokenizer(br.readLine());
+// int a=Integer.parseInt(stk.nextToken());
+// int b=Integer.parseInt(stk.nextToken());
+// System.out.println(a-b);
+// br.readLine();
