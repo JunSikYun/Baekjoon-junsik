8a439bce2a4a839161ad64347877d36bd2007955
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2023-08-16 13:06:04 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 164 ms, Memory: 12652 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 시간복잡도상 Systme.out.prinln
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer stk=new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        for(int i=a;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // int b = Integer.parseInt(stk.nextToken());
+        // System.out.println((a*b));
+    }
+}
+// 97~122 소문자 아스키 코드 값
+// int b=Integer.parseInt(stk.nextToken());
+// System.out.println(a+b);
+// bw.write(String.valueOf(a-b));
+// bw.newLine();//개행 넣어주기
+// bw.write(String.valueOf(a));
+// bw.flush();
+// for(int i=0;i<TC;i++){
+// StringTokenizer stk=new StringTokenizer(br.readLine());
+// int a=Integer.parseInt(stk.nextToken());
+// int b=Integer.parseInt(stk.nextToken());
+// System.out.println(a-b);
+// br.readLine();
