7357baa49fc8d5ac885873870af28dc2be36f664
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2024-01-29 22:17:32 +0900

    [Bronze V] Title: A+B - 2, Time: 92 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer stk = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+        // int A=Integer.parseInt(br.readLine());
+        int A=Integer.parseInt(br.readLine());
+        int B=Integer.parseInt(br.readLine());
+        System.out.print(A+B);
+	}
+}
