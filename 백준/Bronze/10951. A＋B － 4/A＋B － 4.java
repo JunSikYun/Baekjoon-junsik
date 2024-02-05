810ab4204ee6fb10f30dcd4909cc472b58e38b2e
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2024-02-05 14:20:23 +0900

    [Bronze V] Title: A+B - 4, Time: 80 ms, Memory: 11708 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //int T=Integer.parseInt(br.readLine());
+        StringBuilder sb=new StringBuilder();
+        StringTokenizer stk;
+        String str;
+        while((str=br.readLine())!=null){
+            stk=new StringTokenizer(str);
+            int a=Integer.parseInt(stk.nextToken());
+            int b=Integer.parseInt(stk.nextToken());
+            sb.append(a+b).append("\n");
+        }
+        System.out.print(sb);
+        
+    }
+}
