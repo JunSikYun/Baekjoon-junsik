fd5b1272efcc448da3420019d9906cbed94d7870
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2024-02-05 13:02:49 +0900

    [Bronze V] Title: A+B - 8, Time: 88 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T=Integer.parseInt(br.readLine());
+        StringTokenizer stk;
+        int A, B;
+        for(int i=1;i<=T;i++){
+            stk=new StringTokenizer(br.readLine());
+            A=Integer.parseInt(stk.nextToken());
+            B=Integer.parseInt(stk.nextToken());
+            System.out.println("Case #"+i+": "+A+" + "+B+" = "+(A+B));
+        }
+    }
+}
