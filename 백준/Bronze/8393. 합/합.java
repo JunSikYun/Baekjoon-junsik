f90a0682aefcef7d290cc68b98f036c9a3648d48
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2024-02-05 12:11:12 +0900

    [Bronze V] Title: 합, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // StringTokenizer stk = new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(br.readLine());
+        int sum=0;
+        for(int i=1;i<=N;i++){
+            sum+=i;
+        }
+        System.out.print(sum);
+    }
+}
