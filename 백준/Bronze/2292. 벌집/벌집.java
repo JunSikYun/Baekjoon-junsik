2779372da3e14a31e40633bdc4cfdaea151b080c
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2025-01-26 14:03:36 +0900

    [Bronze II] Title: 벌집, Time: 72 ms, Memory: 11448 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		int count = 1; // 겹 수(최소 루트)
+		int range = 2;	// 범위 (최솟값 기준) 
+ 
+		if (N == 1) {
+			System.out.print(1);
+		}
+ 
+		else {
+			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
+				range = range + (6 * count);
+				count++;
+			}
+			System.out.print(count);
+		}
+	}
+}
